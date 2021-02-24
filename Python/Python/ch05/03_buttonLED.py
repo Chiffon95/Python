8fe75c4ff8c9125e2737b08ba04d7a0a87d1c87d
chiffona <apdlrm201216@gmail.com>
2021-02-24 17:09:16 +0900

    03 add

--- Python/Python/ch05/03_buttonLED.py
+++ Python/Python/ch05/03_buttonLED.py
@@ -19,7 +19,7 @@ led_pin = 4
 light_on = False
 
 GPIO.setup(button_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.SETUP(led_pin, GPIO.OUT)
+GPIO.setup(led_pin, GPIO.OUT)
 
 GPIO.add_event_detect(button_pin, GPIO.RISING, callback = button_callback, bouncetime = 300)
 
