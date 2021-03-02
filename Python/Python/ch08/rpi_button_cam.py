8127bce18b183ea4f454d3bcd746989ae51160a1
chiffona <apdlrm201216@gmail.com>
2021-03-02 14:31:02 +0900

    rpi button capture testing

--- Python/Python/ch08/rpi_button_cam.py
+++ Python/Python/ch08/rpi_button_cam.py
@@ -6,7 +6,8 @@ import time
 def button_capture(channel):
     global capture_on
     with picamera.PiCamera() as camera:
-        GPIO.output(led_pin, True)
+        GPIO.output(led_r_pin, False)
+        GPIO.output(led_y_pin, True)
         print("Capturing.....")
         camera.rotation = 180
         camera.resolution = (640, 480)
@@ -14,18 +15,21 @@ def button_capture(channel):
         camera.exposure_mode = 'auto'
         camera.annotate_background = Color('black')
         camera.annotate_text = 'Button Capture Testing~~~'
-        time.sleep(1)
         camera.capture('/home/pi/chiffona/Python/Python/ch08/button_capture.jpg')
-        GPIO.output(led_pin, False)
+        time.sleep(3)
+        GPIO.output(led_y_pin, False)
+        GPIO.output(led_r_pin, True)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)    
 button_pin = 15
-led_pin =4
+led_y_pin = 4
+led_r_pin = 17
 capture_on = False
 
 GPIO.setup(button_pin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(led_r_pin, GPIO.OUT)
+GPIO.setup(led_y_pin, GPIO.OUT)
 
 GPIO.add_event_detect(button_pin, GPIO.RISING, callback = button_capture, bouncetime = 300)
 
